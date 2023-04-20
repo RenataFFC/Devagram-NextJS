613d1ba673ed97aef48d47415fc893c86c5f933b
Renata Fernandes de Franca Carneiro <renatactba@hotmail.com>
2023-04-20 17:33:58 -0300

    Versao inicial login

---
+++ devagram-node-nextjs/pages/api/login.ts
@@ -0,0 +1,20 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+ // eslint-disable-next-line import/no-anonymous-default-export
+ export default(
+    req: NextApiRequest,
+    res: NextApiResponse
+) => {
+ if(req.method === 'POST'){
+  const{login,senha} = req.body;
+     
+    if(login === 'admin@admin.com' && senha === 'Admin@123'){
+            res.status(200).json({msg:'Usuario autenticado com sucesso!'});
+    }
+    return  res.status(405).json({erro:'Usuario e senha nao encontrado!'});
+
+}
+  return res.status(405).json({erro:'Metodo informado não é válido!'});
+
+}
+
+
