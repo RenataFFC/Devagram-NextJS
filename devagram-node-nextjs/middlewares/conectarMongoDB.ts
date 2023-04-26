18198064eba3d9e518492a8b32fe0963cd07a8f0
Renata Fernandes de Franca Carneiro <renatactba@hotmail.com>
2023-04-26 20:12:32 -0300

    conexao login

--- devagram-node-nextjs/middlewares/conectarMongoDB.ts
+++ devagram-node-nextjs/middlewares/conectarMongoDB.ts
@@ -1,32 +1,30 @@
-import type { NextApiRequest, NextApiResponse, NextApiHandler } from "next";
-import mongoose from "mongoose";
-import handler from "@/pages/api/hello";
+import type {NextApiRequest, NextApiResponse, NextApiHandler} from 'next';
+import mongoose from 'mongoose';
+import type {RespostaPadraoMsg} from '../types/RespostaPadraoMsg';
 
 export const conectarMongoDB = (handler : NextApiHandler) =>
- async (req: NextApiRequest , res: NextApiResponse) =>{
+    async (req: NextApiRequest, res : NextApiResponse<RespostaPadraoMsg>) => {
 
-  //Verificar se o banco está conectado, se estiver seguir para o endpoint
-//para o endpoint ou proximo middleware
+    // verificar se o banco ja esta conectado, se estiver seguir 
+    // para o endpoint ou proximo middleware
+    if(mongoose.connections[0].readyState){
+        return handler(req, res);
+    }
 
-if(mongoose.connections[0].readyState){
-  return handler(req,res);
+    // ja que nao esta conectado vamos conectar
+    // obter a variavel de ambiente preenchida do env
+    const {DB_CONEXAO_STRING} = process.env;
 
-}
+    // se a env estiver vazia aborta o uso do sistema e avisa o programador
+    if(!DB_CONEXAO_STRING){
+        return res.status(500).json({ erro : 'ENV de configuracao do banco, nao informado'});
+    }
 
-
-//ja que nao esta conectado vamos conectar
-//obter a variavel de ambiente preenchoda do env
-
-const{DB_CONEXAO_STRING} = process.env;
-
-// se estiver vazia aborta o uso do sistema e avisa o programador
-if(!DB_CONEXAO_STRING){
-    return res.status(500).json({ erro: 'ENV de configuração do banco, não informado'});  
-}
-
-mongoose.connection.on('connectado', () => console.log('Banco de dados conectado'));
-mongoose.connection.on('error', error => console.log(`Ocorreu erro ao conectar no banco: ${error} `))
-await  mongoose.connect(DB_CONEXAO_STRING);
-//AGora posso seguir para o endpoint, pois estou conectado no banco
-return handler(req,res);
+    mongoose.connection.on('connected', () => console.log('Banco de dados conectado'));
+    mongoose.connection.on('error', error => console.log(`Ocorreu erro ao conectar no banco: ${error}`));
+    await mongoose.connect(DB_CONEXAO_STRING);
+    
+    // agora posso seguir para o endpoint, pois estou conectado
+    // no banco
+    return handler(req, res);
 }
--- devagram-node-nextjs/pages/api/login.ts
+++ devagram-node-nextjs/pages/api/login.ts
@@ -1,19 +1,21 @@
-import { conectarMongoDB } from './../../middlewares/conectarMongoDB';
-import type { NextApiRequest, NextApiResponse } from 'next'
+import { conectarMongoDB } from '../../middlewares/conectarMongoDB';
+import type { NextApiRequest, NextApiResponse } from 'next';
+import type { RespostaPadraoMsg } from '../../types/RespostaPadraoMsg';
  // eslint-disable-next-line import/no-anonymous-default-export
 
 
 const endpointLogin = (
     req: NextApiRequest,
-    res: NextApiResponse
+    res: NextApiResponse<RespostaPadraoMsg>
 ) => {
+
  if(req.method === 'POST'){
   const{login,senha} = req.body;
-     
+    
     if(login === 'admin@admin.com' && senha === 'Admin@123'){
           return  res.status(200).json({msg:'Usuario autenticado com sucesso!'});
     }
-    return  res.status(405).json({erro:'Usuario e senha nao encontrado!'});
+    return  res.status(400).json({erro:'Usuario e senha nao encontrado!'});
 
 }
   return res.status(405).json({erro:'Metodo informado não é válido!'});
