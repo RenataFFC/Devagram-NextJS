574720cace35042ab77a43325d0dfdb24f20ff6a
Renata Fernandes de Franca Carneiro <renatactba@hotmail.com>
2023-05-03 18:07:07 -0300

    TOKEN JWL

--- devagram-node-nextjs/pages/api/login.ts
+++ devagram-node-nextjs/pages/api/login.ts
@@ -1,29 +1,42 @@
 import { conectarMongoDB } from '../../middlewares/conectarMongoDB';
 import type { NextApiRequest, NextApiResponse } from 'next';
 import type { RespostaPadraoMsg } from '../../types/RespostaPadraoMsg';
+import type { LoginResposta } from '../../types/LoginResposta';
 import md5 from  'md5';
 import {UsuarioModel} from '../../models/UsuarioModel';
 import { Await } from 'react-router-dom';
+import jwt from 'jsonwebtoken';
  // eslint-disable-next-line import/no-anonymous-default-export
 
 
 const endpointLogin = async(
     req: NextApiRequest,
-    res: NextApiResponse<RespostaPadraoMsg>
+    res: NextApiResponse<RespostaPadraoMsg | LoginResposta>
 ) => {
+
+const {MINHA_CHAVE_JWT} = process.env;
+if(!MINHA_CHAVE_JWT){
+   res.status(500).json({erro: 'ENV Jwt não informado'});
+}
+
 
  if(req.method === 'POST'){
   const{login,senha} = req.body;
 
     const usuariosEncontrados = await UsuarioModel.find({email : login, senha : md5(senha)});
         if(usuariosEncontrados && usuariosEncontrados.length > 0){
-        const usuarioEncontrado = usuariosEncontrados[0];
-        return res.status(200).json ({msg:`Usuário com sucesso`});
-}
+            const usuarioEncontrado = usuariosEncontrados[0];
+
+           const token = jwt.sign({_id: usuarioEncontrado._id}, MINHA_CHAVE_JWT);
+
+          return res.status(200).json({
+                nome : usuarioEncontrado.nome , email: usuarioEncontrado.email, token});
+        }
 return res.status(400).json({erro: 'Usuario ou senha nao encontrado'});
  }
 return res.status(405).json({erro: 'Metodo informado nao é valido'});
 }
+
 
 
 export default conectarMongoDB(endpointLogin);
