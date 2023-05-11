34e5520aa83f6098f96b76f085fa17c095501aa3
Renata Fernandes de Franca Carneiro <renatactba@hotmail.com>
2023-05-11 17:24:36 -0300

    feed

--- devagram-node-nextjs/pages/api/feed.ts
+++ devagram-node-nextjs/pages/api/feed.ts
@@ -2,17 +2,39 @@ import type { NextApiRequest , NextApiResponse} from 'next';
 import type {RespostaPadraoMsg} from '../../types/RespostaPadraoMsg';
 import {validarTokenJWT} from '../../middlewares/validarTokenJWT';
 import {conectarMongoDB} from '../../middlewares/conectarMongoDB';
+import { UsuarioModel} from '../../models/UsuarioModel';
+import { PublicacaoModel } from '../../models/PublicacaoModel';
 
-const feedEndPoint = ( req: NextApiRequest, res: NextApiResponse<RespostaPadraoMsg>)=>{
+const feedEndPoint = async ( req: NextApiRequest, res: NextApiResponse<RespostaPadraoMsg | any>) => {
 
 
 try{
+  if(req.method === 'GET'){
+       //receber informacao do id do usuario que eu quero buscar o feed
+    if(req?.query?.id){
+       // agora tenho o id, como eu valido se o o usuario é valido e como busco as publicacoes
+        const usuario = await UsuarioModel.findById(req?.query?.id);
+
+        if(!usuario){
+        return res.status(400).json({erro:'Usuario não encontrado'})
+        }
+        const publicacoes = await PublicacaoModel
+        .find({idUsuario : usuario._id})
+         .sort({data: -1});
+
+      return res.status(200).json(publicacoes);
+    }
+  }
+
+return res.status(400).json({erro:'Método informado não é válido'})  
 
 
 }catch(e){
   console.log(e)
   res.status(400).json({erro:'nao foi possivel obter o feed'})
 
+}
+}
 
-}
-}
+
+export default validarTokenJWT(conectarMongoDB(feedEndPoint));
