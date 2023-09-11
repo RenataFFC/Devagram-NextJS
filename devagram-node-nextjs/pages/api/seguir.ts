3b581bf3df8f9e17bbbb8fc9b38cd73d9062d47d
Renata Fernandes de Franca Carneiro <renatactba@hotmail.com>
2023-09-11 14:13:18 -0300

    Seguir

--- devagram-node-nextjs/pages/api/seguir.ts
+++ devagram-node-nextjs/pages/api/seguir.ts
@@ -1,3 +1,5 @@
+import moment from "moment";
+import { json } from "stream/consumers";
 import type { NextApiRequest , NextApiResponse} from 'next';
 import type {RespostaPadraoMsg} from '../../types/RespostaPadraoMsg';
 import {validarTokenJWT} from '../../middlewares/validarTokenJWT';
@@ -5,14 +7,19 @@ import {conectarMongoDB} from '../../middlewares/conectarMongoDB';
 import { politicaCORS } from '../../middlewares/politicaCORS';
 import { UsuarioModel } from '../../models/UsuarioModel';
 import { SeguidorModel } from '../../models/SeguidorModels';
+import { NotificacaoModel } from "@/models/NotificacaoModel";
 
 
 const endpointSeguir = 
 async ( req: NextApiRequest, res: NextApiResponse<RespostaPadraoMsg>)=>{
  
  try {
+  // Verifica se o método HTTP é PUT, caso não seja retorna um erro de método inválido
     if(req.method === 'PUT'){
+
     const{userId, id} = req?.query;
+    // Usando um destructor pega na query da request (caso exista) 
+    //as propriedades userID(usuário que vai seguir) e id (Usuário a ser seguido)
 
    // usuario logado/autenticado = quem esta fazendo as acoes
     const usuarioLogado = await  UsuarioModel.findById(userId);
@@ -21,22 +28,30 @@ async ( req: NextApiRequest, res: NextApiResponse<RespostaPadraoMsg>)=>{
     }
 
    // id do usuario e ser seguidor - query
-   const usuarioASerSeguido = await  UsuarioModel.findById(id);
+   const usuarioASerSeguido = await  UsuarioModel.findById(id); // Busca na DB o usuário que vai ser seguido pelo id
    if(!usuarioASerSeguido){
    return res.status(400).json({erro: 'Usuário a ser seguido  não encontrado'});
    }
 
  // buscar se EU LOGADO sigo ou nao esse usuario
- const euJaSigoEsseUsuario =  await  SeguidorModel.find({usuarioId: usuarioLogado._id, usuarioSeguidoId: usuarioASerSeguido._id});
-   if(euJaSigoEsseUsuario && euJaSigoEsseUsuario.length >0){
+ const euJaSigoEsseUsuario =  await  SeguidorModel.find({
+    usuarioId: usuarioLogado._id, 
+    usuarioSeguidoId: usuarioASerSeguido._id,
+  });
 
-   // sinal que eu ja sigo esse usuario
-   euJaSigoEsseUsuario.forEach(async(e: any) => await SeguidorModel.findByIdAndDelete({_id: e._id}));
+   if(euJaSigoEsseUsuario && euJaSigoEsseUsuario.length > 0){ // sinal que eu ja sigo esse usuario   
+       euJaSigoEsseUsuario.forEach ( async(e: any) =>  
+              await SeguidorModel.findByIdAndDelete({_id: e._id}));
    
  usuarioLogado.seguindo--;
- await UsuarioModel.findByIdAndUpdate({_id: usuarioLogado._id}, usuarioLogado);
+
+          await UsuarioModel.findByIdAndUpdate({_id: usuarioLogado._id},
+           usuarioLogado); //atualiza o usuario no banco 
+
  usuarioASerSeguido.seguidores--;
- await UsuarioModel.findByIdAndUpdate({_id: usuarioASerSeguido._id}, usuarioASerSeguido);
+
+           await UsuarioModel.findByIdAndUpdate({_id: usuarioASerSeguido._id}, 
+            usuarioASerSeguido); // Atualiza o usuário a ser desseguido na DB
  
  return res.status(200).json({msg: 'Deixou de seguir o Usuario com sucesso'});
 
@@ -52,8 +67,21 @@ async ( req: NextApiRequest, res: NextApiResponse<RespostaPadraoMsg>)=>{
     usuarioLogado.seguindo++;
     await UsuarioModel.findByIdAndUpdate({_id: usuarioLogado._id}, usuarioLogado);
 
-   usuarioASerSeguido.seguidores++;
-  await UsuarioModel.findByIdAndUpdate({_id: usuarioASerSeguido._id}, usuarioASerSeguido);
+    usuarioASerSeguido.seguidores++;
+   await UsuarioModel.findByIdAndUpdate({_id: usuarioASerSeguido._id}, usuarioASerSeguido);
+
+   const date = Date.now();
+        const NotificacaoCriada = {
+          usuarioAcao: usuarioLogado._id,
+          usuarioNotificado: usuarioASerSeguido._id,
+          tipo: "seguir",
+          dataNotificacao: moment(date).format(),
+          visualizada: false,
+        };
+
+        await NotificacaoModel.create(NotificacaoCriada);
+
+
 
 
     return res.status(200).json({msg: 'Usuario seguido  com sucesso'});
